d37c01e0558d40c15f9ceb21bbd4b9894c4c5c74
binu <110495431+Bini2672@users.noreply.github.com>
2023-12-02 04:05:41 -0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,132 @@
+
+document.getElementById("bytt").onclick = function(){
+
+  let bv =Math.floor(Math.random()* 80)+1;
+  let ka = Math.pow(bv, 2);
+  let mn = Math.sqrt(ka);
+  let pjk = Math.ceil(mn);
+ document.getElementById("i").innerHTML = pjk;
+  let b =Math.floor(Math.random()* 80)+1;
+  let k = Math.pow(b, 2);
+  let m = Math.sqrt(k);
+  let jkl = Math.ceil(m)
+  document.getElementById("id").innerHTML = jkl;
+
+  let obo =Math.floor(Math.random()* 80)+1;
+  let ok = Math.pow(obo, 2);
+  let om = Math.sqrt(ok);
+  let jk = Math.ceil(om);
+  document.getElementById("d").innerHTML = jk;
+
+  let ob =Math.floor(Math.random()* 80)+1;
+  let o = Math.pow(ob, 2);
+  let omo = Math.sqrt(o);
+  let ojk = Math.ceil(omo);
+  document.getElementById("idd").innerHTML = ojk;
+
+  let omb =Math.floor(Math.random()* 80)+1;
+  let oa = Math.pow(omb, 2);
+  let omlo = Math.sqrt(oa);
+  let oojk = Math.ceil(omlo);
+  document.getElementById("ii").innerHTML = oojk;
+
+  let bbv =Math.floor(Math.random()* 80)+1;
+  let bka = Math.pow(bbv, 2);
+  let bmn = Math.sqrt(bka);
+  let bpjk = Math.ceil(bmn);
+ document.getElementById("iid").innerHTML = bpjk;
+
+ let bb =Math.floor(Math.random()* 80)+1;
+  let bk = Math.pow(bb, 2);
+  let bm = Math.sqrt(bk);
+  let bpj = Math.ceil(bm);
+ document.getElementById("dd").innerHTML = bpj;
+
+ let bbc =Math.floor(Math.random()* 80)+1;
+  let bkc = Math.pow(bbc, 2);
+  let bmc = Math.sqrt(bkc);
+  let bpjc = Math.ceil(bmc);
+ document.getElementById("idcd").innerHTML = bpjc;
+
+ let bbvc =Math.floor(Math.random()* 80)+1;
+  let bkac = Math.pow(bbvc, 2);
+  let bmnc = Math.sqrt(bkac);
+  let bpjkc = Math.ceil(bmnc);
+ document.getElementById("iid").innerHTML = bpjkc;
+
+ let abb =Math.floor(Math.random()* 80)+1;
+  let abk = Math.pow(abb, 2);
+  let abm = Math.sqrt(abk);
+  let abpj = Math.ceil(abm);
+ document.getElementById("iod").innerHTML = abpj;
+
+ let cabb =Math.floor(Math.random()* 80)+1;
+ let cabk = Math.pow(cabb, 2);
+ let cabm = Math.sqrt(cabk);
+ let cabpj = Math.ceil(cabm);
+document.getElementById("io").innerHTML = cabpj;
+
+let calb =Math.floor(Math.random()* 80)+1;
+let calbk = Math.pow(calb, 2);
+let calbb = Math.sqrt(calbk);
+let cabp = Math.ceil(calbb);
+document.getElementById("do").innerHTML = cabp;
+
+
+let alb =Math.floor(Math.random()* 80)+1;
+let albk = Math.pow(alb, 2);
+let albb = Math.sqrt(albk);
+let abp = Math.ceil(albb);
+document.getElementById("idod").innerHTML = abp;
+
+let abbv =Math.floor(Math.random()* 80)+1;
+let abkv = Math.pow(abbv, 2);
+let abmv = Math.sqrt(abkv);
+let abpjv = Math.ceil(abmv);
+document.getElementById("ibd").innerHTML = abpjv;
+
+let cbb =Math.floor(Math.random()* 80)+1;
+let cbk = Math.pow(cbb, 2);
+let cbm = Math.sqrt(cbk);
+let cbpj = Math.ceil(cbm);
+document.getElementById("ib").innerHTML = cbpj;
+
+let cxab =Math.floor(Math.random()* 80)+1;
+let cxabk = Math.pow(cxab, 2);
+let cxabb = Math.sqrt(cxabk);
+let xcab = Math.ceil(cxabb);
+document.getElementById("db").innerHTML = xcab;
+
+let aalb =Math.floor(Math.random()* 80)+1;
+let aalbk = Math.pow(aalb, 2);
+let aalbb = Math.sqrt(aalbk);
+let aabp = Math.ceil(aalbb);
+document.getElementById("idbd").innerHTML = aabp;
+
+let abnbv =Math.floor(Math.random()* 80)+1;
+let abnkv = Math.pow(abnbv, 2);
+let abnmv = Math.sqrt(abnkv);
+let abnpjv = Math.ceil(abnmv);
+document.getElementById("idc").innerHTML = abnpjv;
+
+let cbfb =Math.floor(Math.random()* 80)+1;
+let cbfk = Math.pow(cbfb, 2);
+let cbfm = Math.sqrt(cbfk);
+let cbfpj = Math.ceil(cbfm);
+document.getElementById("ic").innerHTML = cbfpj;
+
+let cxadb =Math.floor(Math.random()* 80)+1;
+let cxabdk = Math.pow(cxadb, 2);
+let cxabdb = Math.sqrt(cxabdk);
+let xcadb = Math.ceil(cxabdb);
+document.getElementById("dc").innerHTML = xcadb;
+
+
+let aaslb =Math.floor(Math.random()* 80)+1;
+let aaslbk = Math.pow(aaslb, 2);
+let aaslbb = Math.sqrt(aaslbk);
+let aasbp = Math.ceil(aaslbb);
+document.getElementById("idpd").innerHTML = aasbp;
+
+
+}
